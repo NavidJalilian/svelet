ae686c491ce154452701c03baf553322fc340ddd
Navid Jalilian <n.jalilian@aranumadc.local>
2023-11-12 22:34:28 +0330

    Add bits-ui dependency and update styles.

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+	import Button from "$lib/components/ui/button/button.svelte";
+
 </script>
 
 <h1 class='text-blue-800'>hello</h1>
 
+<Button variant="outline" size="lg">Hello</Button>
